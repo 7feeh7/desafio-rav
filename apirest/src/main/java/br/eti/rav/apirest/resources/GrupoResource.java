010527a61784f51f82fa0a475d6a31947c286143
Felipe Pires <felipe.pires.soaresti@gmail.com>
2020-03-17 01:33:33 -0300

    Alterando e excluindo grupos de tarefas

--- apirest/src/main/java/br/eti/rav/apirest/resources/GrupoResource.java
+++ apirest/src/main/java/br/eti/rav/apirest/resources/GrupoResource.java
@@ -3,8 +3,11 @@ package br.eti.rav.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +31,15 @@ public class GrupoResource {
 	public Grupo save(@RequestBody Grupo grupo) {
 		return grupoRepository.save(grupo);
 	}
+	
+	@PutMapping("/grupos")
+	public Grupo update(@RequestBody Grupo grupo) {
+		return grupoRepository.save(grupo);
+	}
+	
+	@DeleteMapping("/grupos/{id}")
+	public void delete(@PathVariable(value = "id") long id) {
+		grupoRepository.deleteById(id);
+	}
+	
 }
