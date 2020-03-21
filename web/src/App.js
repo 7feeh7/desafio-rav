c52a18f34bed263cdbf5bd349f237b21c2576e22
Felipe Pires <felipe.pires.soaresti@gmail.com>
2020-03-21 19:51:49 -0300

    Definindo componentes do projeto

--- web/src/App.js
+++ web/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Header from './components/Header';
+import Board from './components/Board';
 
 export default function App() {
   return(
     <>
       <Header />
+      <Board />
       <GlobalStyle />
     </>
   ); 
---
+++ web/src/components/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Card() {
+    return (
+        <Container>
+            <p>Fazer migração completa de servidor</p>
+        </Container>
+    );
+}
